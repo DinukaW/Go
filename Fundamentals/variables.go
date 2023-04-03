edd2df5266189dd2933e1a199239267f6664dbee
DinukaW <dinukaw95@gmail.com>
2023-04-03 15:56:51 +0530

    variables declaring and assigning

--- Fundamentals/variables.go
+++ Fundamentals/variables.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Hello Dinuka")
 
+	var game = "last of us" //declaring and assigning variable
+	fmt.Println(game)
+
+	game = "last of us 2" //reassigning
+	fmt.Println(game)
 }
